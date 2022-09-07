9e4b82f3a126818a4404335a1bb717b3bc41f8d4
Michael Hoppes <michael.hoppes@checkr.com>
2022-09-07 11:15:33 -0600

    allow input for notation to generate game; add tests

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   DIRECTIONS = [-9, -8, -7, -1, 1, 7, 8, 9]
+  ASCII_CHR_OFFSET = 97
 
   after_initialize :setup
 
@@ -11,13 +12,35 @@ class Board < ApplicationRecord
   end
 
   def move!(spot)
-    return false unless legal_move?(spot)
+    raise ArgumentError, spot unless legal_move?(spot)
     self.board[spot] = player_turn
     update_board(spot)
     history[spot] = {color: player_turn, order: moves}
     self.turn = !turn
     self.moves += 1
+    skip_turn
     save!
+  end
+
+  def self.coordinate_game(input)
+    game = self.new
+    moves = input.downcase.scan(/.{2}/)
+    moves.each do |m|
+      x = m[0].ord - ASCII_CHR_OFFSET
+      y = (m[1].to_i - 1) * 8
+      game.move!(x + y)
+    end
+    game
+  end
+
+  def legal_moves
+    moves = []
+    (0..63).each do |s|
+      next unless board[s].nil?
+      valid_move = DIRECTIONS.map { |dir| search?(s, dir) }
+      moves << s if valid_move.any?
+    end
+    moves
   end
 
   private
@@ -36,22 +59,15 @@ class Board < ApplicationRecord
       legal_moves.include?(spot)
     end
 
-    def legal_moves
-      moves = []
-      (0..63).each do |s|
-        next unless board[s].nil?
-        moves << s if (DIRECTIONS.map { |dir| search?(s, dir) }).any?
-      end
-      moves
-    end
-
     def search?(s, dir)
+      return false unless should_traverse?(dir, s)
       s += dir
       return false unless s > -1 && s < 64
       return false if board[s].nil?
       return false if board[s] == player_turn
 
       while s > -1 && s < 63
+        return false unless should_traverse?(dir, s)
         s += dir
         return false if board[s].nil?
         return true if board[s] == player_turn
@@ -67,13 +83,54 @@ class Board < ApplicationRecord
 
     def update_board(s)
       DIRECTIONS.each do |dir|
+        next unless should_traverse?(dir, s)
+        ss = s + dir
+        valid = false
+        while ss > -1 && ss < 63 && !valid
+          break if board[ss].nil?
+          valid = true if board[ss] == player_turn
+          break unless should_traverse?(dir, ss)
+          ss += dir
+        end
+
+        next unless valid
+
         ss = s + dir
         while ss > -1 && ss < 63
-          break if board[ss].nil?
-          break if board[ss] == player_turn
+          break if board[ss] == player_turn || board[ss].nil?
           board[ss] = player_turn
+          break unless should_traverse?(dir, ss)
           ss += dir
         end
       end
+    end
+
+    def should_traverse?(dir, s)
+      return false if (s + dir) < 0 || (s + dir) > 63
+
+      case dir
+        when -9
+          !(s % 8).zero?
+        when -8
+          s > 8
+        when -7
+          s % 8 != 7
+        when -1
+          !(s % 8).zero?
+        when 1
+          s % 8 != 7
+        when 7
+          !(s % 8).zero?
+        when 8
+          s < 55
+        when 9
+          s % 8 != 7
+        else
+          raise ArgumentError, "dir #{dir} not supported"
+      end
+    end
+
+    def skip_turn
+      self.turn = !self.turn if legal_moves == []
     end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
+  let(:board) { Board.create }
+  let(:blank_row) { Array.new(8) }
+
   describe 'initial board' do
-    let(:board) { Board.create }
-    let(:blank_row) { Array.new(8) }
     let(:new_board) do
       [
         blank_row,
@@ -14,7 +15,7 @@ RSpec.describe Board, type: :model do
         blank_row,
         blank_row,
         blank_row
-      ]
+      ].flatten
     end
     let(:board_display) do
       <<-OUTPUT
@@ -32,7 +33,7 @@ RSpec.describe Board, type: :model do
     end
 
     it 'creates a new board' do
-      expect(board.board).to eq(new_board.flatten)
+      expect(board.board).to eq(new_board)
     end
 
     it 'display an initial board' do
@@ -54,6 +55,215 @@ RSpec.describe Board, type: :model do
     it 'marks a legal move in history' do
       board.move!(19)
       expect(board.history[19]).to eq(JSON.parse({color: 0, order: 1}.to_json))
+    end
+  end
+
+  describe 'black move a4' do
+    let(:initial_board) do
+      [
+        blank_row,
+        [0, 0, 0, nil, nil, nil, nil, nil],
+        [1, 1, 0, nil, nil, nil, nil, nil],
+        [nil, 1, 0, nil, nil, nil, nil, nil],
+        [1, 1, 0, nil, nil, nil, nil, nil],
+        [0, 0, 0, nil, nil, nil, nil, nil],
+        blank_row,
+        blank_row
+      ].flatten
+    end
+
+    it 'marks all surrounding squares black' do
+      board.board = initial_board
+      board.move!(24)
+      expect(board.board[16]).to eq(0)
+      expect(board.board[17]).to eq(0)
+      expect(board.board[25]).to eq(0)
+      expect(board.board[32]).to eq(0)
+      expect(board.board[33]).to eq(0)
+      (0..7).each { |i| expect(board.board[i]).to be_nil }
+      (4..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (5..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (6..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (7..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (48..63).each { |i| expect(board.board[i]).to be_nil }
+    end
+  end
+
+  describe 'black move h4' do
+    let(:initial_board) do
+      [
+        blank_row,
+        [nil, nil, nil, nil, nil, 0, 0, 0],
+        [nil, nil, nil, nil, nil, 0, 1, 1],
+        [nil, nil, nil, nil, nil, 0, 1, nil],
+        [nil, nil, nil, nil, nil, 0, 1, 1],
+        [nil, nil, nil, nil, nil, 0, 0, 0],
+        blank_row,
+        blank_row
+      ].flatten
+    end
+
+    it 'marks all surrounding squares black' do
+      board.board = initial_board
+      board.move!(31)
+      expect(board.board[22]).to eq(0)
+      expect(board.board[23]).to eq(0)
+      expect(board.board[30]).to eq(0)
+      expect(board.board[38]).to eq(0)
+      expect(board.board[39]).to eq(0)
+      (0..7).each { |i| expect(board.board[i]).to be_nil }
+      (0..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (1..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (2..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (3..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (4..64).step(8).each { |i| expect(board.board[i]).to be_nil }
+      (48..63).each { |i| expect(board.board[i]).to be_nil }
+    end
+  end
+
+  describe 'black move h2' do
+    let(:initial_board) do
+      [
+        [1, 0, 0, 0, 0, 0, 1, nil],
+        [0, 0, 0, 0, 0, 0, nil, nil],
+        [0, 0, 1, 0, 1, 0, 1, nil],
+        [1, 1, 1, 1, 1, 1, 1, nil],
+        [1, 1, 1, 1, 1, 0, 1, nil],
+        [1, 1, 1, 1, 0, 1, 1, nil],
+        [1, 1, 1, 1, 1, 1, 1, nil],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+      ].flatten
+    end
+
+    it 'marks all surrounding squares black' do
+      board.board = initial_board
+      board.move!(15)
+      expect(board.board[22]).to eq(0)
+      expect(board.board[29]).to eq(0)
+      expect(board.board[36]).to eq(0)
+      expect(board.board[43]).to eq(0)
+      expect(board.board[50]).to eq(0)
+
+      expect(board.board[8]).to eq(0)
+
+      expect(board.board[7]).to be_nil
+      expect(board.board[14]).to be_nil
+      expect(board.board[23]).to be_nil
+    end
+  end
+
+  describe 'no moves for black' do
+    let(:initial_board) do
+      [
+        blank_row,
+        blank_row,
+        blank_row,
+        [nil, nil, nil, 0, 0, 0, nil, nil],
+        [nil, nil, nil, 0, 1, 0, nil, nil],
+        [nil, nil, nil, 0, 0, 0, nil, nil],
+        blank_row,
+        blank_row,
+      ].flatten
+    end
+
+    it 'gives no legal moves' do
+      board.board = initial_board
+      expect(board.legal_moves).to eq([])
+    end
+  end
+
+  describe '#coordinate_game' do
+    # http://transcripts.worldothello.org/woc_history/2019/transcripts/round_14/html/TAKAHASHI%20Aki-TAKANASHI%20Yus_2.htm
+    let(:final_board) do
+      [
+        [1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,1,0,1,0,0,0],
+        [1,1,1,1,0,0,0,0],
+        [1,1,1,1,1,1,0,0],
+        [1,1,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,1,0],
+      ].flatten
+      end
+    let(:board_52) do
+      [
+        [1,0,0,0,0,0,1,nil],
+        [0,0,0,0,0,0,nil,nil],
+        [0,0,1,0,1,0,1,nil],
+        [1,1,1,1,1,1,1,nil],
+        [1,1,1,1,1,0,1,nil],
+        [1,1,1,1,0,1,1,nil],
+        [1,1,1,1,1,1,1,nil],
+        [0,0,0,0,0,0,1,0],
+      ].flatten
+    end
+    let(:board_53) do
+      [
+        [1,0,0,0,0,0,1,nil],
+        [0,0,0,0,0,0,nil,0],
+        [0,0,1,0,1,0,0,nil],
+        [1,1,1,1,1,0,1,nil],
+        [1,1,1,1,0,0,1,nil],
+        [1,1,1,0,0,1,1,nil],
+        [1,1,0,1,1,1,1,nil],
+        [0,0,0,0,0,0,1,0],
+      ].flatten
+    end
+    let(:board_54) do
+      [
+        [1,0,0,0,0,0,1,nil],
+        [0,0,0,0,0,0,nil,0],
+        [0,0,1,0,1,1,1,1],
+        [1,1,1,1,1,0,1,nil],
+        [1,1,1,1,0,0,1,nil],
+        [1,1,1,0,0,1,1,nil],
+        [1,1,0,1,1,1,1,nil],
+        [0,0,0,0,0,0,1,0],
+      ].flatten
+    end
+    let(:board_55) do
+      [
+        [1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,nil,0],
+        [0,0,1,0,1,1,1,1],
+        [1,1,1,1,1,0,1,nil],
+        [1,1,1,1,0,0,1,nil],
+        [1,1,1,0,0,1,1,nil],
+        [1,1,0,1,1,1,1,nil],
+        [0,0,0,0,0,0,1,0],
+      ].flatten
+    end
+
+    it 'gives the correct board after move 52' do
+      input = 'f5d6c4d3c3f4f6f3e6e7c6g6g5f7e2c7d8c5b5e8f8b3d7b6e3g3a6g4c8b7a3d2e1c2b1b8b4g7h8a4a8g8f2c1d1g1b2a1f1a7a2a5'
+      board = Board.coordinate_game(input)
+      expect(board.board).to eq(board_52)
+    end
+
+    it 'gives the correct board after move 53' do
+      input = 'f5d6c4d3c3f4f6f3e6e7c6g6g5f7e2c7d8c5b5e8f8b3d7b6e3g3a6g4c8b7a3d2e1c2b1b8b4g7h8a4a8g8f2c1d1g1b2a1f1a7a2a5h2'
+      board = Board.coordinate_game(input)
+      expect(board.board).to eq(board_53)
+    end
+
+    it 'gives the correct board after move 54' do
+      input = 'f5d6c4d3c3f4f6f3e6e7c6g6g5f7e2c7d8c5b5e8f8b3d7b6e3g3a6g4c8b7a3d2e1c2b1b8b4g7h8a4a8g8f2c1d1g1b2a1f1a7a2a5h2h3'
+      board = Board.coordinate_game(input)
+      expect(board.board).to eq(board_54)
+    end
+
+    it 'gives the correct board after move 55' do
+      input = 'f5d6c4d3c3f4f6f3e6e7c6g6g5f7e2c7d8c5b5e8f8b3d7b6e3g3a6g4c8b7a3d2e1c2b1b8b4g7h8a4a8g8f2c1d1g1b2a1f1a7a2a5h2h3h1'
+      board = Board.coordinate_game(input)
+      expect(board.board).to eq(board_55)
+      expect(board.turn).to eq(false)
+    end
+
+    it 'gives the correct final board' do
+      input = 'f5d6c4d3c3f4f6f3e6e7c6g6g5f7e2c7d8c5b5e8f8b3d7b6e3g3a6g4c8b7a3d2e1c2b1b8b4g7h8a4a8g8f2c1d1g1b2a1f1a7a2a5h2h3h1h7h6g2h5h4'
+      board = Board.coordinate_game(input)
+      expect(board.board).to eq(final_board)
     end
   end
 end
